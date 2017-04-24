10c33cd4a7501979c3f11d454c0270c0791dc1d0
John Wu <me@johnwu.cc>
2017-04-25 00:07:49 +0800

    Add Webpack

---
+++ MyWebsite/webpack.config.js
@@ -0,0 +1,41 @@
+﻿/// <binding ProjectOpened='Watch - Development' />
+var webpack = require("webpack");
+var wwwroot = __dirname + "/wwwroot";
+
+module.exports = {
+    cache: true,
+    entry: {
+        "bundle": [wwwroot + "/app/bundle.ts"],
+        "bundle-vendors": [wwwroot + "/app/bundle-vendors.ts"],
+    },
+    output: {
+        path: wwwroot + "/js",
+        filename: "[name].js",
+        sourceMapFilename: "[name].map"
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".html"]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    "awesome-typescript-loader",
+                    "angular2-template-loader"
+                ],
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.html$/,
+                loader: "raw-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "bundle-vendors"
+        })
+    ]
+}
---
+++ MyWebsite/wwwroot/app/bundle-vendors.ts
@@ -0,0 +1,14 @@
+// core
+require("core-js");
+require("zone.js");
+require("rxjs");
+
+// angular
+require("@angular/core");
+require("@angular/common");
+require("@angular/compiler");
+require("@angular/platform-browser");
+require("@angular/platform-browser-dynamic");
+require("@angular/http");
+require("@angular/router");
+require("@angular/forms");
